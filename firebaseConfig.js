a02103628ded317db2622ba1f6e3b0013bd2e074
dannielskkynoff <zipcrafty@gmail.com>
2025-01-04 08:45:41 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,13 @@
-// firebaseConfig.js
-// Adicione seu arquivo de configuração aqui ou simplesmente crie o arquivo com as informações abaixo
-
+// Configurações do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCjDxtzK9ELCyxLNgmd0ZYDhB1o-bPLWr8",
   authDomain: "nuxpro-gestao-de-entregas.firebaseapp.com",
   projectId: "nuxpro-gestao-de-entregas",
-  storageBucket: "nuxpro-gestao-de-entregas.firebasestorage.app",
+  storageBucket: "nuxpro-gestao-de-entregas.appspot.com",
   messagingSenderId: "483551326765",
   appId: "1:483551326765:web:e77ef1e29d69f58efa7ea4",
   measurementId: "G-FVR4EXZR93"
 };
+
+// Inicialize o Firebase
+firebase.initializeApp(firebaseConfig);
