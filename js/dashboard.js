bd666eaecd254558b4397894a2a83108cf488bea
dannielskkynoff <zipcrafty@gmail.com>
2025-01-04 07:00:18 -0300

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,24 +1,37 @@
 function openPopup(tipo) {
     const popup = document.getElementById('popup');
+    const configPopup = document.getElementById('config-popup');
     const popupTitle = document.getElementById('popup-title');
     const trocoContainer = document.getElementById('troco-container');
 
-    popup.style.display = 'flex';
-
     if (tipo === 'chamar') {
+        popup.style.display = 'flex';
         popupTitle.textContent = 'Chamar Entregador';
     } else if (tipo === 'configuracoes') {
-        popupTitle.textContent = 'Configurações';
+        configPopup.style.display = 'flex';
     } else if (tipo === 'perfil') {
+        popup.style.display = 'flex';
         popupTitle.textContent = 'Ver Perfil';
     } else if (tipo === 'relatorios') {
+        popup.style.display = 'flex';
         popupTitle.textContent = 'Ver Relatórios';
     }
 }
 
 function fecharPopup() {
     document.getElementById('popup').style.display = 'none';
+    document.getElementById('config-popup').style.display = 'none';
 }
+
+document.getElementById('theme-switch').addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('dark-theme');
+        document.body.classList.remove('light-theme');
+    } else {
+        document.body.classList.add('light-theme');
+        document.body.classList.remove('dark-theme');
+    }
+});
 
 document.getElementById('popup-form').addEventListener('submit', function (event) {
     event.preventDefault();
