6c9fe7c3f574ee7c2543e939852f4e58819959ae
dannielskkynoff <zipcrafty@gmail.com>
2025-01-04 09:31:09 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,13 @@
-// Configurações do Firebase
+// Configuração do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCjDxtzK9ELCyxLNgmd0ZYDhB1o-bPLWr8",
-  authDomain: "nuxpro-gestao-de-entregas.firebaseapp.com",
-  projectId: "nuxpro-gestao-de-entregas",
-  storageBucket: "nuxpro-gestao-de-entregas.appspot.com",
-  messagingSenderId: "483551326765",
-  appId: "1:483551326765:web:e77ef1e29d69f58efa7ea4",
-  measurementId: "G-FVR4EXZR93"
+    apiKey: "AIzaSyCjDxtzK9ELCyxLNgmd0ZYDhB1o-bPLWr8",
+    authDomain: "nuxpro-gestao-de-entregas.firebaseapp.com",
+    projectId: "nuxpro-gestao-de-entregas",
+    storageBucket: "nuxpro-gestao-de-entregas.appspot.com",
+    messagingSenderId: "483551326765",
+    appId: "1:483551326765:web:e77ef1e29d69f58efa7ea4",
+    measurementId: "G-FVR4EXZR93"
 };
 
-// Inicialize o Firebase
+// Inicializa o Firebase
 firebase.initializeApp(firebaseConfig);
